0356340bf8db39e40f6b25b24ab0118218036cc3
Anton Gushcha <ncrashed@gmail.com>
2015-09-06 13:42:12 +0300

    Another pack of binded methods of Node class

--- src/Graphics/Urho3D/Scene/Node.hs
+++ src/Graphics/Urho3D/Scene/Node.hs
@@ -68,6 +68,21 @@ module Graphics.Urho3D.Scene.Node(
   , nodeSetEnabledRecursive
   , nodeSetOwner
   , nodeMarkDirty
+  , nodeCreateChild
+  , nodeCreateChildSimple
+  , nodeAddChild
+  , nodeRemoveChild
+  , nodeRemoveAllChildren
+  , nodeRemoveChildren
+  , nodeCreateComponent
+  , nodeGetOrCreateComponent
+  , nodeCloneComponent
+  , nodeCloneComponent'
+  , nodeRemoveComponent
+  , nodeRemoveComponent'
+  , nodeRemoveComponents
+  , nodeRemoveComponents'
+  , nodeRemoveAllComponents
   -- | Getters
   , nodeGetComponent
   , nodeGetRotation
@@ -684,3 +699,157 @@ nodeMarkDirty :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer 
 nodeMarkDirty p = liftIO $ do 
   let ptr = parentPointer p 
   [C.exp| void { $(Node* ptr)->MarkDirty() } |]
+
+-- | Create a child scene node (with specified ID if provided).
+nodeCreateChild :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> String -- ^ name, default ""
+  -> CreateMode -- ^ mode, default replicated 
+  -> Int -- ^ id, default 0 
+  -> m (Ptr Node)
+nodeCreateChild p name mode i = liftIO $ withCString name $ \name' -> do 
+  let ptr = parentPointer p 
+      mode' = fromIntegral $ fromEnum mode 
+      i' = fromIntegral i 
+  [C.exp| Node* { $(Node* ptr)->CreateChild(String($(const char* name')), (CreateMode)$(int mode'), $(int i')) } |]
+
+-- | Create a child scene node
+nodeCreateChildSimple :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> m (Ptr Node)
+nodeCreateChildSimple p = nodeCreateChild p "" CM'Replicated 0
+
+-- | Add a child scene node at a specific index. If index is not explicitly specified or is greater than current children size, append the new child at the end.
+nodeAddChild :: (Parent Node a, Pointer p a, Parent Node child, Pointer pChild child, MonadIO m) => p -- ^ Node pointer or child
+  -> pChild -- ^ pointer to child node
+  -> Maybe Int -- ^ maybe an index
+  -> m ()
+nodeAddChild p c mi = liftIO $ do 
+  let ptr = parentPointer p 
+      child = parentPointer c 
+      i = maybe [C.pure| int { M_MAX_UNSIGNED } |] fromIntegral mi
+  [C.exp| void { $(Node* ptr)->AddChild($(Node* child), $(int i)) } |]
+
+-- | Remove a child scene node.
+nodeRemoveChild :: (Parent Node a, Pointer p a, Parent Node child, Pointer pChild child, MonadIO m) => p -- ^ Node pointer or child
+  -> pChild -- ^ pointer to child node
+  -> m ()
+nodeRemoveChild p c = liftIO $ do 
+  let ptr = parentPointer p 
+      child = parentPointer c 
+  [C.exp| void { $(Node* ptr)->RemoveChild($(Node* child)) } |]
+
+-- | Remove all child scene nodes
+nodeRemoveAllChildren :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> m ()
+nodeRemoveAllChildren p = liftIO $ do 
+  let ptr = parentPointer p 
+  [C.exp| void { $(Node* ptr)->RemoveAllChildren() } |]
+
+-- | Remove child scene nodes that match criteria
+nodeRemoveChildren :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Bool -- ^ removeReplicated
+  -> Bool -- ^ removeLocal
+  -> Bool -- ^ recursive
+  -> m ()
+nodeRemoveChildren p removeReplicated removeLocal recursive = liftIO $ do 
+  let ptr = parentPointer p 
+      r' = fromBool removeReplicated
+      l' = fromBool removeLocal
+      rec' = fromBool recursive
+  [C.exp| void { $(Node* ptr)->RemoveChildren($(int r') != 0, $(int l') != 0, $(int rec') != 0) } |]
+
+-- | Create a component to this node (with specified ID if provided)
+nodeCreateComponent :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Ptr StringHash -- ^ type 
+  -> Maybe CreateMode -- ^ mode, default is replicated
+  -> Maybe Int -- ^ id, default is 0 
+  -> m (Maybe (Ptr Component))
+nodeCreateComponent p th mc mi = liftIO $ do 
+  let ptr = parentPointer p 
+      c' = maybe [C.pure| int { (int)REPLICATED } |] (fromIntegral . fromEnum) mc 
+      i' = maybe 0 fromIntegral mi 
+  cp <- [C.exp| Component* { $(Node* ptr)->CreateComponent(*$(StringHash* th), (CreateMode)$(int c'), $(int i')) } |]
+  checkNullPtr' cp return
+
+-- | Create a component to this node if it does not exist already.
+nodeGetOrCreateComponent :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Ptr StringHash -- ^ type 
+  -> Maybe CreateMode -- ^ mode, default is replicated
+  -> Maybe Int -- ^ id, default is 0 
+  -> m (Maybe (Ptr Component))
+nodeGetOrCreateComponent p th mc mi = liftIO $ do 
+  let ptr = parentPointer p 
+      c' = maybe [C.pure| int { (int)REPLICATED } |] (fromIntegral . fromEnum) mc 
+      i' = maybe 0 fromIntegral mi 
+  cp <- [C.exp| Component* { $(Node* ptr)->GetOrCreateComponent(*$(StringHash* th), (CreateMode)$(int c'), $(int i')) } |]
+  checkNullPtr' cp return
+
+-- |  Clone a component from another node using its create mode. Return the clone if successful or null on failure.
+nodeCloneComponent :: (Parent Node a, Pointer p a, Parent Component cmp, Pointer pComponent cmp, MonadIO m) => p -- ^ Node pointer or child
+  -> pComponent -- ^ Component pointer 
+  -> Maybe Int -- ^ id, default 0 
+  -> m (Maybe (Ptr Component))
+nodeCloneComponent p pCmp mi = liftIO $ do 
+  let ptr = parentPointer p 
+      cmp = parentPointer pCmp 
+      i' = maybe 0 fromIntegral mi
+  cp <- [C.exp| Component* { $(Node* ptr)->CloneComponent($(Component* cmp), $(int i')) } |]
+  checkNullPtr' cp return
+
+-- | Clone a component from another node and specify the create mode. Return the clone if successful or null on failure.
+nodeCloneComponent' :: (Parent Node a, Pointer p a, Parent Component cmp, Pointer pComponent cmp, MonadIO m) => p -- ^ Node pointer or child
+  -> pComponent -- ^ Component pointer 
+  -> CreateMode -- ^ mode
+  -> Maybe Int -- ^ id, default 0 
+  -> m (Maybe (Ptr Component))
+nodeCloneComponent' p pCmp cm mi = liftIO $ do 
+  let ptr = parentPointer p 
+      cmp = parentPointer pCmp 
+      cm' = fromIntegral $ fromEnum cm
+      i' = maybe 0 fromIntegral mi
+  cp <- [C.exp| Component* { $(Node* ptr)->CloneComponent($(Component* cmp), (CreateMode)$(int cm'), $(int i')) } |]
+  checkNullPtr' cp return
+
+-- | Remove a component from this node.
+nodeRemoveComponent :: (Parent Node a, Pointer p a, Parent Component cmp, Pointer pComponent cmp, MonadIO m) => p -- ^ Node pointer or child
+  -> pComponent -- ^ Component pointer 
+  -> m ()
+nodeRemoveComponent p pCmp = liftIO $ do 
+  let ptr = parentPointer p 
+      cmp = parentPointer pCmp 
+  [C.exp| void { $(Node* ptr)->RemoveComponent($(Component* cmp)) } |] 
+
+-- | Remove the first component of specific type from this node.
+nodeRemoveComponent' :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Ptr StringHash -- ^ type
+  -> m ()
+nodeRemoveComponent' p ph = liftIO $ do 
+  let ptr = parentPointer p 
+  [C.exp| void { $(Node* ptr)->RemoveComponent(*$(StringHash* ph)) } |]
+
+-- | Remove components that match criteria.
+nodeRemoveComponents :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Bool -- ^ removeReplicated
+  -> Bool -- ^ removeLocal
+  -> m ()
+nodeRemoveComponents p removeReplicated removeLocal = liftIO $ do 
+  let ptr = parentPointer p 
+      r' = fromBool removeReplicated
+      l' = fromBool removeLocal
+  [C.exp| void { $(Node* ptr)->RemoveComponents($(int r') != 0, $(int l') != 0)} |]
+
+-- | Remove all components of specific type.
+nodeRemoveComponents' :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> Ptr StringHash -- ^ type
+  -> m ()
+nodeRemoveComponents' p ph = liftIO $ do 
+  let ptr = parentPointer p 
+  [C.exp| void { $(Node* ptr)->RemoveComponents(*$(StringHash* ph)) } |]
+
+-- | Remove all components from this node.
+nodeRemoveAllComponents :: (Parent Node a, Pointer p a, MonadIO m) => p -- ^ Node pointer or child
+  -> m ()
+nodeRemoveAllComponents p = liftIO $ do 
+  let ptr = parentPointer p 
+  [C.exp| void { $(Node* ptr)->RemoveAllComponents() } |]
+
+-- Stopped at: https://github.com/urho3d/Urho3D/blob/master/Source/Urho3D/Scene/Node.h#L274
