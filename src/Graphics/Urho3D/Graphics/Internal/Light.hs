aaf329dc126e93e08273d415c67c3656e6703dcc
NCrashed <ncrashed@gmail.com>
2016-05-02 21:01:22 +0300

    Light support types

--- src/Graphics/Urho3D/Graphics/Internal/Light.hs
+++ src/Graphics/Urho3D/Graphics/Internal/Light.hs
@@ -1,6 +1,22 @@
 module Graphics.Urho3D.Graphics.Internal.Light(
     Light
   , PODVectorLightPtr
+  , BiasParameters(..)
+  , HasBias(..)
+  , HasSlopeScaleBias(..)
+  , CascadeParameters(..)
+  , HasSplit1(..)
+  , HasSplit2(..)
+  , HasSplit3(..)
+  , HasSplit4(..)
+  , HasFadeStart(..)
+  , HasBiasAutoAdjust(..)
+  , FocusParameters(..)
+  , HasFocus(..)
+  , HasNonUniform(..)
+  , HasAutoSize(..)
+  , HasQuantize(..)
+  , HasMinView(..)
   , lightCntx
   ) where
 
@@ -8,15 +24,55 @@ import qualified Language.C.Inline as C
 import qualified Language.C.Inline.Context as C
 import qualified Language.C.Types as C
 
+import Control.DeepSeq 
+import Control.Lens 
+import GHC.Generics 
 import qualified Data.Map as Map
 
 data Light 
 data PODVectorLightPtr
 
+-- | Shadow depth bias parameters.
+data BiasParameters = BiasParameters {
+  _biasParametersBias :: {-# UNPACK #-} !Float -- ^ Constant bias.
+, _biasParametersSlopeScaleBias :: {-# UNPACK #-} !Float -- ^ Slope scaled bias.
+} deriving (Eq, Show, Generic)
+
+makeFields ''BiasParameters
+instance NFData BiasParameters
+
+-- | Cascaded shadow map parameters.
+data CascadeParameters = CascadeParameters {
+  _cascadeParametersSplit1 :: {-# UNPACK #-} !Float -- ^ Far clip values of the splits.
+, _cascadeParametersSplit2 :: {-# UNPACK #-} !Float -- ^ Far clip values of the splits.
+, _cascadeParametersSplit3 :: {-# UNPACK #-} !Float -- ^ Far clip values of the splits.
+, _cascadeParametersSplit4 :: {-# UNPACK #-} !Float -- ^ Far clip values of the splits.
+, _cascadeParametersFadeStart :: {-# UNPACK #-} !Float -- ^ The point relative to the total shadow range where shadow fade begins (0.0 - 1.0)
+, _cascadeParametersBiasAutoAdjust :: {-# UNPACK #-} !Float -- ^ Automatic depth bias adjustment strength.
+} deriving (Eq, Show, Generic)
+
+makeFields ''CascadeParameters
+instance NFData CascadeParameters
+
+-- | Shadow map focusing parameters.
+data FocusParameters = FocusParameters {
+  _focusParametersFocus :: !Bool -- ^ Focus flag.
+, _focusParametersNonUniform :: !Bool -- ^ Non-uniform focusing flag.
+, _focusParametersAutoSize :: !Bool -- ^ Auto-size (reduce resolution when far away) flag.
+, _focusParametersQuantize :: {-# UNPACK #-} !Float -- ^ Focus quantization.
+, _focusParametersMinView :: {-# UNPACK #-} !Float -- ^ Minimum view size.
+} deriving (Eq, Show, Generic)
+
+makeFields ''FocusParameters
+instance NFData FocusParameters
+
 lightCntx :: C.Context 
 lightCntx = mempty {
     C.ctxTypesTable = Map.fromList [
       (C.TypeName "Light", [t| Light |])
+    , (C.TypeName "BiasParameters", [t| BiasParameters |])
+    , (C.TypeName "CascadeParameters", [t| CascadeParameters |])
+    , (C.TypeName "FocusParameters", [t| FocusParameters |])
     , (C.TypeName "PODVectorLightPtr", [t| PODVectorLightPtr |])
     ]
   } 
--- src/Graphics/Urho3D/Graphics/Light.hs
+++ src/Graphics/Urho3D/Graphics/Light.hs
@@ -2,6 +2,22 @@
 module Graphics.Urho3D.Graphics.Light(
     Light
   , PODVectorLightPtr
+  , BiasParameters(..)
+  , HasBias(..)
+  , HasSlopeScaleBias(..)
+  , CascadeParameters(..)
+  , HasSplit1(..)
+  , HasSplit2(..)
+  , HasSplit3(..)
+  , HasSplit4(..)
+  , HasFadeStart(..)
+  , HasBiasAutoAdjust(..)
+  , FocusParameters(..)
+  , HasFocus(..)
+  , HasNonUniform(..)
+  , HasAutoSize(..)
+  , HasQuantize(..)
+  , HasMinView(..)
   , LightType(..)
   , lightContext
   , lightSetLightType
@@ -15,6 +31,7 @@ import Graphics.Urho3D.Graphics.Internal.Light
 import Graphics.Urho3D.Monad
 import Data.Monoid
 import System.IO.Unsafe (unsafePerformIO) 
+import Foreign 
 
 import Graphics.Urho3D.Math.StringHash 
 import Graphics.Urho3D.Scene.Node
@@ -26,10 +43,26 @@ import Graphics.Urho3D.Scene.Animatable
 import Graphics.Urho3D.Graphics.Internal.Drawable 
 import Graphics.Urho3D.Scene.Component 
 import Graphics.Urho3D.Parent
+import Text.RawString.QQ
 
 C.context (C.cppCtx <> lightCntx <> componentContext <> stringHashContext <> drawableCntx <> animatableContext <> serializableContext <> objectContext)
 C.include "<Urho3D/Graphics/Light.h>"
 C.using "namespace Urho3D"
+
+C.verbatim [r|
+template <class T>
+class Traits
+{
+public:
+    struct AlignmentFinder
+    {
+      char a; 
+      T b;
+    };
+
+    enum {AlignmentOf = sizeof(AlignmentFinder) - sizeof(T)};
+};
+|]
 
 podVectorPtr "Light"
 
@@ -50,6 +83,72 @@ data LightType =
   | LT'Spot
   | LT'Point
   deriving (Eq, Show, Read, Enum, Bounded)
+
+instance Storable BiasParameters where 
+  sizeOf _ = fromIntegral $ [C.pure| int { (int)sizeof(BiasParameters) } |]
+  alignment _ = fromIntegral $ [C.pure| int { (int)Traits<BiasParameters>::AlignmentOf } |]
+  peek ptr = do 
+    _biasParametersBias <- realToFrac <$> [C.exp| float {$(BiasParameters* ptr)->constantBias_} |]
+    _biasParametersSlopeScaleBias <- realToFrac <$> [C.exp| float {$(BiasParameters* ptr)->slopeScaledBias_} |]
+    return BiasParameters {..}
+  poke ptr (BiasParameters {..}) = [C.block| void { 
+      $(BiasParameters* ptr)->constantBias_ = $(float _biasParametersBias');
+      $(BiasParameters* ptr)->slopeScaledBias_ = $(float _biasParametersSlopeScaleBias');
+    } |]
+    where
+    _biasParametersBias' = realToFrac _biasParametersBias
+    _biasParametersSlopeScaleBias' = realToFrac _biasParametersSlopeScaleBias
+
+instance Storable CascadeParameters where 
+  sizeOf _ = fromIntegral $ [C.pure| int { (int)sizeof(CascadeParameters) } |]
+  alignment _ = fromIntegral $ [C.pure| int { (int)Traits<CascadeParameters>::AlignmentOf } |]
+  peek ptr = do 
+    _cascadeParametersSplit1 <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->splits_[0] } |]
+    _cascadeParametersSplit2 <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->splits_[1] } |]
+    _cascadeParametersSplit3 <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->splits_[2] } |]
+    _cascadeParametersSplit4 <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->splits_[3] } |]
+    _cascadeParametersFadeStart <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->fadeStart_ } |]
+    _cascadeParametersBiasAutoAdjust <- realToFrac <$> [C.exp| float { $(CascadeParameters* ptr)->biasAutoAdjust_ } |]
+    return CascadeParameters {..}
+  poke ptr (CascadeParameters {..}) = [C.block| void { 
+      $(CascadeParameters* ptr)->splits_[0] = $(float _cascadeParametersSplit1');
+      $(CascadeParameters* ptr)->splits_[1] = $(float _cascadeParametersSplit2');
+      $(CascadeParameters* ptr)->splits_[2] = $(float _cascadeParametersSplit3');
+      $(CascadeParameters* ptr)->splits_[3] = $(float _cascadeParametersSplit4');
+      $(CascadeParameters* ptr)->fadeStart_ = $(float _cascadeParametersFadeStart');
+      $(CascadeParameters* ptr)->biasAutoAdjust_ = $(float _cascadeParametersBiasAutoAdjust');
+    } |]
+    where
+    _cascadeParametersSplit1' = realToFrac _cascadeParametersSplit1
+    _cascadeParametersSplit2' = realToFrac _cascadeParametersSplit2
+    _cascadeParametersSplit3' = realToFrac _cascadeParametersSplit3
+    _cascadeParametersSplit4' = realToFrac _cascadeParametersSplit4
+    _cascadeParametersFadeStart' = realToFrac _cascadeParametersFadeStart
+    _cascadeParametersBiasAutoAdjust' = realToFrac _cascadeParametersBiasAutoAdjust
+
+instance Storable FocusParameters where 
+  sizeOf _ = fromIntegral $ [C.pure| int { (int)sizeof(FocusParameters) } |]
+  alignment _ = fromIntegral $ [C.pure| int { (int)Traits<FocusParameters>::AlignmentOf } |]
+  peek ptr = do 
+    _focusParametersFocus <- toBool <$> [C.exp| int { (int)$(FocusParameters* ptr)->focus_ } |]
+    _focusParametersNonUniform <- toBool <$> [C.exp| int { (int)$(FocusParameters* ptr)->nonUniform_ } |]
+    _focusParametersAutoSize <- toBool <$> [C.exp| int { (int)$(FocusParameters* ptr)->autoSize_ } |]
+    _focusParametersQuantize <- realToFrac <$> [C.exp| float { $(FocusParameters* ptr)->quantize_ } |]
+    _focusParametersMinView <- realToFrac <$> [C.exp| float { $(FocusParameters* ptr)->minView_ } |]
+    return FocusParameters {..}
+  poke ptr (FocusParameters {..}) = [C.block| void { 
+      $(FocusParameters* ptr)->focus_ = $(int _focusParametersFocus') != 0;
+      $(FocusParameters* ptr)->nonUniform_ = $(int _focusParametersNonUniform') != 0;
+      $(FocusParameters* ptr)->autoSize_ = $(int _focusParametersAutoSize') != 0;
+      $(FocusParameters* ptr)->quantize_ = $(float _focusParametersQuantize');
+      $(FocusParameters* ptr)->minView_ = $(float _focusParametersMinView');
+    } |]
+    where
+    _focusParametersFocus' = fromBool _focusParametersFocus
+    _focusParametersNonUniform' = fromBool _focusParametersNonUniform
+    _focusParametersAutoSize' = fromBool _focusParametersAutoSize
+    _focusParametersQuantize' = realToFrac _focusParametersQuantize
+    _focusParametersMinView' = realToFrac _focusParametersMinView
 
 -- | Set light type
 lightSetLightType :: (Parent Light a, Pointer p a, MonadIO m) 
