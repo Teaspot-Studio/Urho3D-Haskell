aada7734f85d842ee8a745e9fd200007d1745701
Anton Gushcha <ncrashed@gmail.com>
2017-07-19 17:29:18 +0300

    Add ord instance to rects

--- src/Graphics/Urho3D/Math/Internal/Rect.hs
+++ src/Graphics/Urho3D/Math/Internal/Rect.hs
@@ -15,27 +15,27 @@ import qualified Language.C.Inline.Context as C
 import qualified Language.C.Types as C
 import qualified Data.Map as Map
 import Graphics.Urho3D.Math.Internal.Vector2
-import Control.Lens 
-import GHC.Generics 
+import Control.Lens
+import GHC.Generics
 import Control.DeepSeq
 
 data Rect = Rect {
-  _rectMinPoint :: {-# UNPACK #-} !Vector2 
-, _rectMaxPoint :: {-# UNPACK #-} !Vector2  
-} deriving (Eq, Generic, Show)
+  _rectMinPoint :: {-# UNPACK #-} !Vector2
+, _rectMaxPoint :: {-# UNPACK #-} !Vector2
+} deriving (Eq, Ord, Generic, Show)
 makeFields ''Rect
-instance NFData Rect 
+instance NFData Rect
 
 data IntRect = IntRect {
-  _intRectLeft :: {-# UNPACK #-} !Int 
-, _intRectTop :: {-# UNPACK #-} !Int 
-, _intRectRight :: {-# UNPACK #-} !Int 
+  _intRectLeft :: {-# UNPACK #-} !Int
+, _intRectTop :: {-# UNPACK #-} !Int
+, _intRectRight :: {-# UNPACK #-} !Int
 , _intRectBottom :: {-# UNPACK #-} !Int
-} deriving (Eq, Generic, Show)
+} deriving (Eq, Ord, Generic, Show)
 makeFields ''IntRect
-instance NFData IntRect 
+instance NFData IntRect
 
-rectCntx :: C.Context 
+rectCntx :: C.Context
 rectCntx = mempty {
     C.ctxTypesTable = Map.fromList [
       (C.TypeName "Rect", [t| Rect |])
