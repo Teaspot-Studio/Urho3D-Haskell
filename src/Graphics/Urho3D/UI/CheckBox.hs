16401d250b20463b9fd9cbd951b127de787d78fd
Anton Gushcha <ncrashed@gmail.com>
2017-07-23 19:57:03 +0300

    Check box functions

--- src/Graphics/Urho3D/UI/CheckBox.hs
+++ src/Graphics/Urho3D/UI/CheckBox.hs
@@ -3,6 +3,10 @@ module Graphics.Urho3D.UI.CheckBox(
     CheckBox
   , checkBoxContext
   , SharedCheckBox
+  , checkBoxSetChecked
+  , checkBoxSetCheckedOffset
+  , checkBoxIsChecked
+  , checkBoxGetCheckedOffset
   ) where
 
 import qualified Language.C.Inline as C
@@ -19,18 +23,29 @@ import System.IO.Unsafe (unsafePerformIO)
 
 import Graphics.Urho3D.Core.Object
 import Graphics.Urho3D.Math.StringHash
+import Graphics.Urho3D.Math.Vector2
 import Graphics.Urho3D.Parent
 import Graphics.Urho3D.Scene.Animatable
 import Graphics.Urho3D.Scene.Serializable
 import Graphics.Urho3D.UI.BorderImage
 import Graphics.Urho3D.UI.Element
 
-C.context (C.cppCtx <> sharedCheckBoxPtrCntx <> checkBoxCntx <> contextContext <> uiElementContext <> borderImageContext <> animatableContext <> serializableContext <> objectContext)
+C.context (C.cppCtx
+  <> sharedCheckBoxPtrCntx
+  <> checkBoxCntx
+  <> contextContext
+  <> uiElementContext
+  <> borderImageContext
+  <> animatableContext
+  <> serializableContext
+  <> objectContext
+  <> vector2Context)
 C.include "<Urho3D/UI/CheckBox.h>"
 C.using "namespace Urho3D"
 
 checkBoxContext :: C.Context
-checkBoxContext = sharedCheckBoxPtrCntx <> checkBoxCntx
+checkBoxContext = sharedCheckBoxPtrCntx
+  <> checkBoxCntx
 
 instance Creatable (Ptr CheckBox) where
   type CreationOptions (Ptr CheckBox) = Ptr Context
@@ -45,3 +60,42 @@ instance UIElem CheckBox where
     unsigned int { CheckBox::GetTypeStatic().Value() } |]
 
 sharedPtr "CheckBox"
+
+-- | Set checked state.
+-- void SetChecked(bool enable);
+checkBoxSetChecked :: (Parent CheckBox a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ Pointer to CheckBox or ascentor
+  -> Bool -- ^ enable
+  -> m ()
+checkBoxSetChecked p v = liftIO $ do
+  let ptr = parentPointer p
+      v' = fromBool v
+  [C.exp| void {$(CheckBox* ptr)->SetChecked($(int v') != 0)} |]
+
+-- | Set checked image offset.
+-- void SetCheckedOffset(const IntVector2& rect);
+checkBoxSetCheckedOffset :: (Parent CheckBox a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ Pointer to CheckBox or ascentor
+  -> IntVector2 -- ^ rect
+  -> m ()
+checkBoxSetCheckedOffset p v = liftIO $ with v $ \v' -> do
+  let ptr = parentPointer p
+  [C.exp| void {$(CheckBox* ptr)->SetCheckedOffset(*$(IntVector2* v'))} |]
+
+-- | Return whether is checked.
+-- bool IsChecked() const { return checked_; }
+checkBoxIsChecked :: (Parent CheckBox a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ Pointer to CheckBox or ascentor
+  -> m Bool
+checkBoxIsChecked p = liftIO $ do
+  let ptr = parentPointer p
+  toBool <$> [C.exp| int {(int)$(CheckBox* ptr)->IsChecked()} |]
+
+-- | Return checked image offset.
+-- const IntVector2& GetCheckedOffset() const { return checkedOffset_; }
+checkBoxGetCheckedOffset :: (Parent CheckBox a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ Pointer to CheckBox or ascentor
+  -> m IntVector2
+checkBoxGetCheckedOffset p = liftIO $ do
+  let ptr = parentPointer p
+  peek =<< [C.exp| const IntVector2* {&$(CheckBox* ptr)->GetCheckedOffset()} |]
