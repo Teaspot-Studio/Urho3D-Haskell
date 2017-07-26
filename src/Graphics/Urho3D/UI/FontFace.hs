195d00e96f5c1c510cdaf5e7b07fbd2eaf243a25
Anton Gushcha <ncrashed@gmail.com>
2017-07-26 18:02:46 +0300

    Add functions to font face

--- src/Graphics/Urho3D/UI/FontFace.hs
+++ src/Graphics/Urho3D/UI/FontFace.hs
@@ -13,6 +13,11 @@ module Graphics.Urho3D.UI.FontFace(
   , HasAdvanceX(..)
   , HasPage(..)
   , HasUsed(..)
+  , fontFaceGetKerning
+  , fontFaceIsDataLost
+  , fontFaceGetPointSize
+  , fontFaceGetRowHeight
+  , fontFaceGetTextures
   ) where
 
 import qualified Language.C.Inline as C
@@ -20,10 +25,17 @@ import qualified Language.C.Inline.Cpp as C
 
 import Data.Monoid
 import Foreign
+import Graphics.Urho3D.Container.ForeignVector
+import Graphics.Urho3D.Container.Ptr
+import Graphics.Urho3D.Graphics.Texture2D
+import Graphics.Urho3D.Monad
 import Graphics.Urho3D.UI.Internal.FontFace
 import Text.RawString.QQ
 
-C.context (C.cppCtx <> fontFaceCntx)
+C.context (C.cppCtx
+  <> fontFaceCntx
+  <> texture2DContext
+  )
 C.include "<Urho3D/Container/Ptr.h>"
 C.include "<Urho3D/Container/HashMap.h>"
 C.include "<Urho3D/UI/FontFace.h>"
@@ -31,6 +43,8 @@ C.using "namespace Urho3D"
 
 fontFaceContext :: C.Context
 fontFaceContext = fontFaceCntx
+
+C.verbatim "typedef Vector<SharedPtr<Texture2D> > VectorSharedTexture2DPtr;"
 
 C.verbatim [r|
 template <class T>
@@ -82,3 +96,52 @@ instance Storable FontGlyph where
     _fontGlyphAdvanceX' = fromIntegral _fontGlyphAdvanceX
     _fontGlyphPage' = fromIntegral _fontGlyphPage
     _fontGlyphUsed' = fromBool _fontGlyphUsed
+
+-- | Return the kerning for a character and the next character.
+-- short GetKerning(unsigned c, unsigned d) const;
+fontFaceGetKerning :: (Parent FontFace a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ pointer to FontFace or ascentor
+  -> Word -- ^ c
+  -> Word -- ^ d
+  -> m Word16
+fontFaceGetKerning p c d = liftIO $ do
+  let ptr = parentPointer p
+      c' = fromIntegral c
+      d' = fromIntegral d
+  fromIntegral <$> [C.exp| short { $(FontFace* ptr)->GetKerning($(unsigned int c'), $(unsigned int d')) } |]
+
+-- | Return true when one of the texture has a data loss.
+-- bool IsDataLost() const;
+fontFaceIsDataLost :: (Parent FontFace a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ pointer to FontFace or ascentor
+  -> m Bool
+fontFaceIsDataLost p = liftIO $ do
+  let ptr = parentPointer p
+  toBool <$> [C.exp| int { (int)$(FontFace* ptr)->IsDataLost() } |]
+
+-- | Return point size.
+-- int GetPointSize() const { return pointSize_; }
+fontFaceGetPointSize :: (Parent FontFace a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ pointer to FontFace or ascentor
+  -> m Int
+fontFaceGetPointSize p = liftIO $ do
+  let ptr = parentPointer p
+  fromIntegral <$> [C.exp| int { $(FontFace* ptr)->GetPointSize() } |]
+
+-- | Return row height.
+-- int GetRowHeight() const { return rowHeight_; }
+fontFaceGetRowHeight :: (Parent FontFace a, Pointer ptr a, MonadIO m)
+  => ptr -- ^ pointer to FontFace or ascentor
+  -> m Int
+fontFaceGetRowHeight p = liftIO $ do
+  let ptr = parentPointer p
+  fromIntegral <$> [C.exp| int{ $(FontFace* ptr)->GetRowHeight() } |]
+
+-- | Return textures.
+-- const Vector<SharedPtr<Texture2D> >& GetTextures() const { return textures_; }
+fontFaceGetTextures :: (Parent FontFace a, Pointer ptr a, MonadIO m, ForeignVector v (SharedPtr Texture2D))
+  => ptr -- ^ pointer to FontFace or ascentor
+  -> m (v (SharedPtr Texture2D))
+fontFaceGetTextures p = liftIO $ do
+  let ptr = parentPointer p
+  peekForeignVectorAs =<< [C.exp| const VectorSharedTexture2DPtr* { &$(FontFace* ptr)->GetTextures() } |]
